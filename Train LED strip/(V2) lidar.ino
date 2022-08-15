4a215263e2c008856cdf64860a162cb559fc78b3
Angel J <59521991+AOD555@users.noreply.github.com>
2022-08-15 16:22:12 -0600

    Create (V2) lidar.ino

---
+++ Train LED strip/(V2) lidar.ino
@@ -0,0 +1,58 @@
+/*------------------------------------------------------------------------------
+
+  LIDARLite Arduino Library
+  v3/GetDistanceI2c
+
+  This example shows how to initialize, configure, and read distance from a
+  LIDAR-Lite connected over the I2C interface.
+
+  Connections:
+  LIDAR-Lite 5 Vdc (red) to Arduino 5v
+  LIDAR-Lite I2C SCL (green) to Arduino SCL
+  LIDAR-Lite I2C SDA (blue) to Arduino SDA
+  LIDAR-Lite Ground (black) to Arduino GND
+
+  (Capacitor recommended to mitigate inrush current when device is enabled)
+  680uF capacitor (+) to Arduino 5v
+  680uF capacitor (-) to Arduino GND
+
+  See the Operation Manual for wiring diagrams and more information:
+  http://static.garmin.com/pumac/LIDAR_Lite_v3_Operation_Manual_and_Technical_Specifications.pdf
+
+------------------------------------------------------------------------------*/
+
+
+
+
+
+void lidar()
+{
+  /*
+    distance(bool biasCorrection, char lidarliteAddress)
+
+    Take a distance measurement and read the result.
+
+    Parameters
+    ----------------------------------------------------------------------------
+    biasCorrection: Default true. Take aquisition with receiver bias
+      correction. If set to false measurements will be faster. Receiver bias
+      correction must be performed periodically. (e.g. 1 out of every 100
+      readings).
+    lidarliteAddress: Default 0x62. Fill in new address here if changed. See
+      operating manual for instructions.
+  */
+
+  // Take a measurement with receiver bias correction and print to serial terminal
+  lidarReturn = myLidarLite.distance();
+  // Serial.println(myLidarLite.distance());
+
+  // Take 99 measurements without receiver bias correction and print to serial terminal
+  // for(int i = 0; i < 99; i++)
+  // {
+  //   Serial.println(myLidarLite.distance(false));
+  //   if(i > 30)
+  //   {
+  //     break;
+  //   }
+  // }
+}
