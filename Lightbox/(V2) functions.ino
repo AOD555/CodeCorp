8357148f072e1508a53630a8544bfb927d411556
Angel J <59521991+AOD555@users.noreply.github.com>
2022-10-20 11:34:01 -0600

    Create (V2) functions.ino
    This is the function calls.

---
+++ Lightbox/(V2) functions.ino
@@ -0,0 +1,138 @@
+
+void photoresistor()
+{
+  light = analogRead(photoPin);
+  Serial.print(" The light level is ");
+  Serial.println(light);
+  Serial.write(light);
+  delay(100);
+
+}
+void resetHBridge()
+{
+
+  digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, LOW);
+	
+	delay(1000);
+}
+const unsigned int MAX_MESSAGE_LENGTH = 10;
+static char message[MAX_MESSAGE_LENGTH];
+
+
+void hidserver_Read()
+  {
+    
+    int i = 0;
+    // photoresistor();
+    
+    while (!Serial.available());
+    delay(100);   // VERY IMPORTANT to give time to read all the write commands from python
+    
+    if (trigger == 0)
+    {
+      // resetting the values to zero
+      // array[0] = 0;
+      // array[1] = 0;
+      delay(100);
+      
+      for(i = 0; i < 4 ; i++)
+      {
+
+        char inByte = Serial.read();
+        if(inByte == '\n')
+        {
+          trigger = 1;
+          break;
+        }
+        else
+        {
+          message[i] = inByte;        // The 'i' is the position the byte is moving into. The posiions for Sub are S -> 1 u -> 2 b -> 3
+        }
+        
+        
+      }
+      message[i] = '\0';
+      int number = atoi(message);   // Converts string input to int
+      array[0] = number;
+      
+      if(array[0] > 255){array[0] = 255;}
+      firstLight = array[0];
+      
+      
+      
+      delay(100);
+    }
+    
+    
+    else if (trigger == 1)
+    {
+      // array[0] = 0;
+      
+      for(i = 0; i < 4 ; i++)
+      {
+        char inByte = Serial.read();
+        if(inByte == '\n')
+        {
+          trigger = 0;
+          Serial.flush();
+          break;
+        }
+        else
+        {
+          message[i] = inByte;        // The 'i' is the position the byte is moving into. The posiions for Sub are S -> 1 u -> 2 b -> 3
+        }
+        
+        
+      }
+      message[i] = '\0';
+      int number = atoi(message);   // Converts string input to int
+      array[1] = number;
+      
+      if(array[1] > 255){array[1] = 255;}
+      secondLight = array[1];
+      
+      delay(100);
+      
+    }
+    
+    
+ 
+    
+   
+  }
+  
+
+// This function lets you control direction of PWM waves
+void directionControl(int input) {
+// For PWM maximum possible values are 0 to 255
+	analogWrite(enA, 255);
+	analogWrite(enB, 255);
+
+	// Turn on Light A & B via H bridge 
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, HIGH);  
+	Serial.println("\nChanging direction\n"); // Changing PWM direction
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, HIGH);
+}
+
+// This function will set the light level according to the input
+void lightControl(int input) {
+	// Turn on Lights via H bridge
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, HIGH);
+	
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, HIGH);
+
+  
+	analogWrite(enA, firstLight);
+	analogWrite(enB, secondLight);
+	Serial.println(firstLight);
+	Serial.println(secondLight);
+}
+
+  
